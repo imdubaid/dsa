cc197d2ef9ddeeae5f5ef3c14db1242d3a01af29
Mohd Ubaid Khan <theubaidahmed@gmail.com>
2025-12-30 16:40:08 +0530

    Sorting: Implement bubble sort algorithm

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,15 @@
+input = [13, 46, 24, 52, 20, 9]
+
+
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+print("Bubble sort: ", bubble_sort(input))
