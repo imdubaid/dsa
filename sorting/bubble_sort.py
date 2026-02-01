7b1c0bf32db58345a75029f356130b87b92f23c9
Mohd Ubaid Khan <theubaidahmed@gmail.com>
2026-02-01 17:19:59 +0530

    Sorting: Add recursive insertion sort , bubble sort implementation

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -12,4 +12,19 @@ def bubble_sort(arr):
     return arr
 
 
+def recursive_bubble_sort(arr, n=None):
+    if n is None:
+        n = len(arr)
+
+    if n == 1:
+        return arr
+
+    for i in range(n - 1):
+        if arr[i] > arr[i + 1]:
+            arr[i], arr[i + 1] = arr[i + 1], arr[i]
+
+    return recursive_bubble_sort(arr, n - 1)
+
+
 print("Bubble sort: ", bubble_sort(input))
+print("Recursive bubble sort: ", recursive_bubble_sort(input.copy()))
--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -6,14 +6,30 @@ def insertion_sort(arr):
 
     for i in range(1, n):
         key = arr[i]
-        j = i - 1
+        prevIndex: int = i - 1
 
-        while j >= 0 and arr[j] > key:
-            arr[j + 1] = arr[j]
-            j -= 1
+        while prevIndex >= 0 and arr[prevIndex] > key:
+            arr[prevIndex + 1] = arr[prevIndex]
+            prevIndex -= 1
 
-        arr[j + 1] = key
+        arr[prevIndex + 1] = key
     return arr
 
 
+def recursive_insertion_sort(arr, n=0):
+    if n == len(arr):
+        return arr
+
+    key = arr[n]
+    prevIndex = n - 1
+
+    while prevIndex >= 0 and arr[prevIndex] > key:
+        arr[prevIndex + 1] = arr[prevIndex]
+        prevIndex -= 1
+
+    arr[prevIndex + 1] = key
+    return recursive_insertion_sort(arr, n + 1)
+
+
 print("Insertion sort: ", insertion_sort(input))
+print("Recursive insertion sort: ", recursive_insertion_sort(input.copy()))
