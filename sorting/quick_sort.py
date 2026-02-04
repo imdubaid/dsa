3b5e26e0321f02b1a0819f4eb3558863d32d139e
Mohd Ubaid Khan <theubaidahmed@gmail.com>
2026-02-04 22:39:56 +0530

    Sorting: Implement quick sort algorithm

---
+++ sorting/quick_sort.py
@@ -0,0 +1,30 @@
+arr = [13, 46, 24, 52, 20, 9]
+
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+def quick_sort(arr, low=0, high=None):
+    if high is None:
+        high = len(arr) - 1
+
+    if low < high:
+        pi = partition(arr, low, high)
+
+        quick_sort(arr, low, pi - 1)
+        quick_sort(arr, pi + 1, high)
+
+    return arr
+
+
+print("Quick sort: ", quick_sort(arr))
