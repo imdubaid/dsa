22e5cec0a6fd229c59aecd2665afebca66c826f7
Mohd Ubaid Khan <theubaidahmed@gmail.com>
2026-02-12 16:41:57 +0530

    Linked List: Single linked list created

---
+++ linked_list/ll.py
@@ -0,0 +1,110 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self, head=None):
+        self.head = head
+
+    def add(self, data, position=None):
+        new_node = Node(data)
+
+        if position is None:
+            self.add_last(data)
+            return
+
+        if position == 0:
+            self.add_first(data)
+            return
+
+        current_node = self.head
+        current_position = 0
+
+        while current_node.next and current_position < position - 1:
+            current_node = current_node.next
+            current_position += 1
+
+        new_node.next = current_node.next
+        current_node.next = new_node
+
+    def add_first(self, data):
+        new_node = Node(data, self.head)
+        self.head = new_node
+
+    def add_middle(self, data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        length = 0
+
+        while current_node.next:
+            current_node = current_node.next
+            length += 1
+
+        mid = length // 2 if length % 2 == 0 else (length + 1) // 2
+        current_node = self.head
+
+        while mid > 1:
+            current_node = current_node.next
+            mid -= 1
+
+        new_node.next = current_node.next
+        current_node.next = new_node
+
+    def add_last(self, data):
+        new_node = Node(data)
+
+        if not self.head:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+
+    def remove(self, key):
+        current_node = self.head
+        previous_node = None
+
+        while current_node.next and current_node.data != key:
+            previous_node = current_node
+            current_node = current_node.next
+
+        if current_node.data != key:
+            return
+
+        if not previous_node:
+            self.head = current_node.next  # Deleting the head node
+        else:
+            previous_node.next = current_node.next  # Bypass the node to delete
+
+    def display(self):
+        elements = []
+        current_node = self.head
+        while current_node:
+            elements.append(current_node.data)
+            current_node = current_node.next
+        print(" -> ".join(map(str, elements)))
+
+
+linked_list = LinkedList()
+
+for num in arr:
+    linked_list.add_last(num)
+
+linked_list.add_first(7)
+linked_list.add_first(17)
+linked_list.add_middle(5)
+linked_list.add_middle(25)
+linked_list.remove(9)
+linked_list.display()
